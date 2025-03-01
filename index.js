948750c5d7fb499bf4df95936afcd1d665b79bd9
Sharon Chepngeno <chepngenokorir19@gmail.com>
2025-03-01 10:41:11 +0300

    complete labs

---
+++ index.js
@@ -0,0 +1,51 @@
+function add(a, b) {
+    return a + b;
+}
+
+console.log(add(4, 3));
+
+function subtract(a, b) {
+    return a - b;
+}
+
+console.log(subtract(6, 4));
+
+
+function multiply(a, b) {
+    return a * b;
+}
+
+console.log(multiply(3, 5 ));
+
+
+function divide(a, b) {
+    return a / b;
+}
+
+console.log(divide(12,3));
+
+
+function increment(n) {
+    return n + 1;
+}
+
+console.log(increment(9))
+
+function decrement(n) {
+    return n - 1;
+}
+
+console.log(decrement(4))
+
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+
+console.log(makeInt('abc'))
+
+
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
+
+console.log(preserveDecimal('abc'))
